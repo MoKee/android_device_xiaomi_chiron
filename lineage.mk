8696e2b6534d760535e870315f3071b0f29dca10
xiaobai <fengz181x@126.com>
2017-09-27 01:54:54 +0800

    chiron: Initialize chiron device tree

---
+++ lineage.mk
@@ -0,0 +1,12 @@
+$(call inherit-product, device/xiaomi/chiron/full_chiron.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_chiron
+PRODUCT_DEVICE := chiron
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MIX 2
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
