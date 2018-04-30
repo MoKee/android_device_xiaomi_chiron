1ef99819b2f163706033ed0b0e361a87f5ea4505
Martincz Gao <martincz@mokeedev.com>
2018-05-01 03:11:35 +0800

    chiron: mokee-ify
    Change-Id: Ic048987586168b77b6fb25cdd3c08870c63c33fd

--- mokee.mk
+++ mokee.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/xiaomi/chiron/full_chiron.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_chiron
+PRODUCT_NAME := mk_chiron
 PRODUCT_DEVICE := chiron
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2
